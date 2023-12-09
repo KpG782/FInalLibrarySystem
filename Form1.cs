df6dda68c39f9487e60035c65a9250c26571dfd7
KpG782 <66615710+KpG782@users.noreply.github.com>
2023-12-09 19:54:20 +0800

    backend start

--- Form1.cs
+++ Form1.cs
@@ -18,12 +18,16 @@ namespace FInalLibrarySystem
         {
             public string Username { get; set; }
             public string Password { get; set; }
+
+            public string FirstName { get; set; }
+            public string LastName { get; set; }
+
+            public string Role { get; set; }
+            public string Department { get; set; }
         }
 
         // Array to store user information
-        private User[] users = new User[10];
-
-
+        private User[] users = new User[20];
 
 
         public frmLogin()
@@ -31,22 +35,24 @@ namespace FInalLibrarySystem
             InitializeComponent();
             InitializeUsers();
 
-
         }
 
         private void InitializeUsers()
         {
             // Initialize the array with sample username and password pairs
-            users[0] = new User { Username = "user1", Password = "pass1" };
-            users[1] = new User { Username = "user2", Password = "pass2" };
-            users[2] = new User { Username = "user3", Password = "pass3" };
-            users[3] = new User { Username = "user4", Password = "pass4" };
-            users[4] = new User { Username = "user5", Password = "pass5" };
-            users[5] = new User { Username = "user6", Password = "pass6" };
-            users[6] = new User { Username = "user7", Password = "pass7" };
-            users[7] = new User { Username = "user8", Password = "pass8" };
-            users[8] = new User { Username = "user9", Password = "pass9" };
-            users[9] = new User { Username = "user10", Password = "pass10" };
+            users[0] = new User { Username = "stud1", Password = "pass1", FirstName = "Ken Patrick", LastName = "Garcia" };
+            users[1] = new User { Username = "stud2", Password = "pass2" };
+            users[2] = new User { Username = "stud3", Password = "pass3" };
+            users[3] = new User { Username = "stud4", Password = "pass4" };
+            users[4] = new User { Username = "stud5", Password = "pass5" };
+            users[5] = new User { Username = "stud6", Password = "pass6" };
+            users[6] = new User { Username = "stud7", Password = "pass7" };
+            users[7] = new User { Username = "stud8", Password = "pass8" };
+            users[8] = new User { Username = "stud9", Password = "pass9" };
+            users[9] = new User { Username = "stud10", Password = "pass10" };
+            users[10] = new User { Username = "teach1", Password = "pass11" };
+            users[11] = new User { Username = "teach2", Password = "pass12" };
+            users[12] = new User { Username = "teach3", Password = "pass13" };
         }
 
         private void Form1_Load(object sender, EventArgs e)
@@ -86,7 +92,7 @@ namespace FInalLibrarySystem
             {
                 if (user.Username == enteredUsername && user.Password == enteredPassword)
                 {
-                    MessageBox.Show("Login successful!");
+                    MessageBox.Show("Login successful! Mr. " + user.FirstName);
 
                     //opens book list form
                     Form3 f3 = new Form3();
