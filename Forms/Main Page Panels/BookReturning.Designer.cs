a6759a3ab559ac923711e2cc2f2ceb36c5ab598e
Lanz Eron Corpuz <lnzcrpz584@gmail.com>
2023-12-30 15:58:27 +0800

    try new

--- Forms/Main Page Panels/BookReturning.Designer.cs
+++ Forms/Main Page Panels/BookReturning.Designer.cs
@@ -28,221 +28,210 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.MainPanel = new System.Windows.Forms.Panel();
             this.panel1 = new System.Windows.Forms.Panel();
+            this.StudentIDPictureBox = new System.Windows.Forms.PictureBox();
             this.SearchByComboBox = new System.Windows.Forms.ComboBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.NameLabel = new System.Windows.Forms.Label();
             this.panel2 = new System.Windows.Forms.Panel();
-            this.SearchTextBox = new System.Windows.Forms.TextBox();
-            this.panel3 = new System.Windows.Forms.Panel();
-            this.dataGridView1 = new System.Windows.Forms.DataGridView();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.BookPictureBox = new System.Windows.Forms.PictureBox();
+            this.BookTitleLinkLabel = new System.Windows.Forms.LinkLabel();
             this.ReturnButton = new System.Windows.Forms.Button();
             this.ClearButton = new System.Windows.Forms.Button();
-            this.BookTitleLabel = new System.Windows.Forms.Label();
-            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.Column5 = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.Column6 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.MainPanel.SuspendLayout();
             this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.StudentIDPictureBox)).BeginInit();
             this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.BookPictureBox)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
+            // 
+            // MainPanel
+            // 
+            this.MainPanel.Controls.Add(this.dataGridView1);
+            this.MainPanel.Controls.Add(this.panel2);
+            this.MainPanel.Controls.Add(this.panel1);
+            this.MainPanel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.MainPanel.Location = new System.Drawing.Point(0, 0);
+            this.MainPanel.Name = "MainPanel";
+            this.MainPanel.Size = new System.Drawing.Size(1268, 1058);
+            this.MainPanel.TabIndex = 0;
             // 
             // panel1
             // 
-            this.panel1.Controls.Add(this.SearchTextBox);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.NameLabel);
+            this.panel1.Controls.Add(this.textBox1);
             this.panel1.Controls.Add(this.SearchByComboBox);
-            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Controls.Add(this.StudentIDPictureBox);
+            this.panel1.Location = new System.Drawing.Point(3, 3);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(635, 408);
+            this.panel1.Size = new System.Drawing.Size(630, 690);
             this.panel1.TabIndex = 0;
-            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint_1);
+            // 
+            // StudentIDPictureBox
+            // 
+            this.StudentIDPictureBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.StudentIDPictureBox.Location = new System.Drawing.Point(26, 37);
+            this.StudentIDPictureBox.Name = "StudentIDPictureBox";
+            this.StudentIDPictureBox.Size = new System.Drawing.Size(243, 311);
+            this.StudentIDPictureBox.TabIndex = 3;
+            this.StudentIDPictureBox.TabStop = false;
             // 
             // SearchByComboBox
             // 
-            this.SearchByComboBox.DropDownWidth = 106;
-            this.SearchByComboBox.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.SearchByComboBox.Font = new System.Drawing.Font("Segoe UI", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.SearchByComboBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.SearchByComboBox.FormattingEnabled = true;
-            this.SearchByComboBox.Items.AddRange(new object[] {
-            "ISSUE ID",
-            "BOOK ID",
-            "STUDENT ID"});
-            this.SearchByComboBox.Location = new System.Drawing.Point(175, 133);
+            this.SearchByComboBox.Location = new System.Drawing.Point(275, 37);
             this.SearchByComboBox.Name = "SearchByComboBox";
-            this.SearchByComboBox.Size = new System.Drawing.Size(106, 25);
-            this.SearchByComboBox.TabIndex = 0;
+            this.SearchByComboBox.Size = new System.Drawing.Size(121, 24);
+            this.SearchByComboBox.TabIndex = 4;
             this.SearchByComboBox.Text = "Search By:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(402, 38);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(186, 24);
+            this.textBox1.TabIndex = 5;
+            // 
+            // NameLabel
+            // 
+            this.NameLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.NameLabel.Location = new System.Drawing.Point(275, 102);
+            this.NameLabel.Name = "NameLabel";
+            this.NameLabel.Size = new System.Drawing.Size(100, 23);
+            this.NameLabel.TabIndex = 6;
+            this.NameLabel.Text = "Name*";
+            this.NameLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // panel2
             // 
-            this.panel2.Controls.Add(this.BookTitleLabel);
+            this.panel2.Controls.Add(this.BookTitleLinkLabel);
             this.panel2.Controls.Add(this.ClearButton);
+            this.panel2.Controls.Add(this.BookPictureBox);
             this.panel2.Controls.Add(this.ReturnButton);
-            this.panel2.Controls.Add(this.pictureBox1);
-            this.panel2.Location = new System.Drawing.Point(633, 0);
+            this.panel2.Location = new System.Drawing.Point(636, 3);
             this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(635, 635);
+            this.panel2.Size = new System.Drawing.Size(630, 690);
             this.panel2.TabIndex = 1;
             // 
-            // SearchTextBox
+            // BookPictureBox
             // 
-            this.SearchTextBox.Location = new System.Drawing.Point(113, 217);
-            this.SearchTextBox.Name = "SearchTextBox";
-            this.SearchTextBox.Size = new System.Drawing.Size(344, 22);
-            this.SearchTextBox.TabIndex = 1;
+            this.BookPictureBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.BookPictureBox.Location = new System.Drawing.Point(88, 37);
+            this.BookPictureBox.Name = "BookPictureBox";
+            this.BookPictureBox.Size = new System.Drawing.Size(470, 470);
+            this.BookPictureBox.TabIndex = 2;
+            this.BookPictureBox.TabStop = false;
             // 
-            // panel3
+            // BookTitleLinkLabel
             // 
-            this.panel3.Location = new System.Drawing.Point(0, 407);
-            this.panel3.Name = "panel3";
-            this.panel3.Size = new System.Drawing.Size(635, 228);
-            this.panel3.TabIndex = 2;
-            // 
-            // dataGridView1
-            // 
-            dataGridViewCellStyle1.BackColor = System.Drawing.Color.Transparent;
-            dataGridViewCellStyle1.NullValue = null;
-            this.dataGridView1.AlternatingRowsDefaultCellStyle = dataGridViewCellStyle1;
-            this.dataGridView1.BorderStyle = System.Windows.Forms.BorderStyle.None;
-            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
-            this.Column1,
-            this.Column2,
-            this.Column3,
-            this.Column4,
-            this.Column5,
-            this.Column6});
-            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Bottom;
-            this.dataGridView1.Location = new System.Drawing.Point(0, 635);
-            this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.RowHeadersWidth = 100;
-            this.dataGridView1.RowTemplate.Height = 24;
-            this.dataGridView1.Size = new System.Drawing.Size(1268, 423);
-            this.dataGridView1.TabIndex = 3;
-            // 
-            // pictureBox1
-            // 
-            this.pictureBox1.Location = new System.Drawing.Point(135, 81);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(370, 327);
-            this.pictureBox1.TabIndex = 0;
-            this.pictureBox1.TabStop = false;
+            this.BookTitleLinkLabel.ActiveLinkColor = System.Drawing.Color.Black;
+            this.BookTitleLinkLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BookTitleLinkLabel.LinkColor = System.Drawing.Color.Black;
+            this.BookTitleLinkLabel.Location = new System.Drawing.Point(15, 522);
+            this.BookTitleLinkLabel.Name = "BookTitleLinkLabel";
+            this.BookTitleLinkLabel.Size = new System.Drawing.Size(599, 23);
+            this.BookTitleLinkLabel.TabIndex = 4;
+            this.BookTitleLinkLabel.TabStop = true;
+            this.BookTitleLinkLabel.Text = "Book Title";
+            this.BookTitleLinkLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.BookTitleLinkLabel.VisitedLinkColor = System.Drawing.Color.Black;
             // 
             // ReturnButton
             // 
-            this.ReturnButton.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.ReturnButton.Location = new System.Drawing.Point(190, 509);
+            this.ReturnButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ReturnButton.Location = new System.Drawing.Point(149, 596);
             this.ReturnButton.Name = "ReturnButton";
-            this.ReturnButton.Size = new System.Drawing.Size(115, 37);
-            this.ReturnButton.TabIndex = 1;
+            this.ReturnButton.Size = new System.Drawing.Size(140, 38);
+            this.ReturnButton.TabIndex = 0;
             this.ReturnButton.Text = "RETURN";
             this.ReturnButton.UseVisualStyleBackColor = true;
-            this.ReturnButton.Click += new System.EventHandler(this.button1_Click);
             // 
             // ClearButton
             // 
-            this.ClearButton.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.ClearButton.Location = new System.Drawing.Point(359, 509);
+            this.ClearButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ClearButton.Location = new System.Drawing.Point(370, 596);
             this.ClearButton.Name = "ClearButton";
-            this.ClearButton.Size = new System.Drawing.Size(115, 37);
-            this.ClearButton.TabIndex = 2;
+            this.ClearButton.Size = new System.Drawing.Size(140, 38);
+            this.ClearButton.TabIndex = 3;
             this.ClearButton.Text = "CLEAR";
             this.ClearButton.UseVisualStyleBackColor = true;
             // 
-            // BookTitleLabel
+            // label1
             // 
-            this.BookTitleLabel.Font = new System.Drawing.Font("Segoe UI", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.BookTitleLabel.Location = new System.Drawing.Point(163, 426);
-            this.BookTitleLabel.Name = "BookTitleLabel";
-            this.BookTitleLabel.Size = new System.Drawing.Size(320, 23);
-            this.BookTitleLabel.TabIndex = 3;
-            this.BookTitleLabel.Text = "Book Title";
-            this.BookTitleLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(275, 145);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(100, 23);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Name*";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
-            // Column1
+            // label2
             // 
-            this.Column1.HeaderText = "Column1";
-            this.Column1.MinimumWidth = 6;
-            this.Column1.Name = "Column1";
-            this.Column1.Width = 125;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(275, 191);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(100, 23);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Name*";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
-            // Column2
+            // dataGridView1
             // 
-            this.Column2.HeaderText = "Column2";
-            this.Column2.MinimumWidth = 6;
-            this.Column2.Name = "Column2";
-            this.Column2.Width = 125;
-            // 
-            // Column3
-            // 
-            this.Column3.HeaderText = "Column3";
-            this.Column3.MinimumWidth = 6;
-            this.Column3.Name = "Column3";
-            this.Column3.Width = 125;
-            // 
-            // Column4
-            // 
-            this.Column4.HeaderText = "Column4";
-            this.Column4.MinimumWidth = 6;
-            this.Column4.Name = "Column4";
-            this.Column4.Width = 125;
-            // 
-            // Column5
-            // 
-            this.Column5.HeaderText = "Column5";
-            this.Column5.MinimumWidth = 6;
-            this.Column5.Name = "Column5";
-            this.Column5.Width = 125;
-            // 
-            // Column6
-            // 
-            this.Column6.HeaderText = "Column6";
-            this.Column6.MinimumWidth = 6;
-            this.Column6.Name = "Column6";
-            this.Column6.Width = 125;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(3, 699);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(1262, 356);
+            this.dataGridView1.TabIndex = 2;
             // 
             // BookReturning
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
-            this.Controls.Add(this.dataGridView1);
-            this.Controls.Add(this.panel2);
-            this.Controls.Add(this.panel1);
-            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.MainPanel);
             this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "BookReturning";
             this.Size = new System.Drawing.Size(1268, 1058);
+            this.MainPanel.ResumeLayout(false);
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.StudentIDPictureBox)).EndInit();
             this.panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.BookPictureBox)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Panel panel1;
-        private System.Windows.Forms.Panel panel2;
-        private System.Windows.Forms.ComboBox SearchByComboBox;
-        private System.Windows.Forms.TextBox SearchTextBox;
-        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Panel MainPanel;
         private System.Windows.Forms.DataGridView dataGridView1;
-        private System.Windows.Forms.Button ReturnButton;
-        private System.Windows.Forms.PictureBox pictureBox1;
-        private System.Windows.Forms.Label BookTitleLabel;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.LinkLabel BookTitleLinkLabel;
         private System.Windows.Forms.Button ClearButton;
-        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
-        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
-        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
-        private System.Windows.Forms.DataGridViewTextBoxColumn Column4;
-        private System.Windows.Forms.DataGridViewTextBoxColumn Column5;
-        private System.Windows.Forms.DataGridViewTextBoxColumn Column6;
+        private System.Windows.Forms.PictureBox BookPictureBox;
+        private System.Windows.Forms.Button ReturnButton;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label NameLabel;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ComboBox SearchByComboBox;
+        private System.Windows.Forms.PictureBox StudentIDPictureBox;
     }
 }
