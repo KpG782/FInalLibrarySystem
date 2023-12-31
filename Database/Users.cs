e011b727c0e6e1bd7ef95c39fb67dfa8ab7ac6c1
KpG782 <66615710+KpG782@users.noreply.github.com>
2023-12-31 21:15:50 +0800

    login and signup final na nagana na, may checker na rin

--- Database/Users.cs
+++ Database/Users.cs
@@ -28,6 +28,9 @@ namespace FInalLibrarySystem.Database
             public string Section { get; set; }
             public string Year { get; set; }
             public byte[] Picture { get; set; }
+
+            public int Money { get; set; }
+       
         }
 
         
@@ -72,7 +75,8 @@ namespace FInalLibrarySystem.Database
                                 EmployeeID = row["employeeID"].ToString(),
                                 Section = row["section"].ToString(),
                                 Year = row["year"].ToString(),
-                                Picture = (byte[])row["picture"]
+                                Picture = (byte[])row["picture"],
+                                Money = Convert.ToInt32( row["money"])
 
                             };
                         }
@@ -263,5 +267,150 @@ namespace FInalLibrarySystem.Database
             return teachers;
         }
 
+
+        public void InsertUserIntoDatabase(User user)
+        {
+            try
+            {
+                using (MySqlConnection connection = db.getConnection())
+                {
+                    db.openConnection(); // Open the database connection
+
+                    // Use a parameterized INSERT query to avoid SQL injection
+                    string query = "INSERT INTO app_users (username, password, email, first_name, last_name, middle_initial, role, " +
+                                   "department, studentID, employeeID, section, year, money) " +
+                                   "VALUES (@username, @password, @email, @firstName, @lastName, @middleInitial, " +
+                                   "@role, @department, @studentID, @employeeID, @section, @year, @money)";
+
+                    using (MySqlCommand cmd = new MySqlCommand(query, connection))
+                    {
+                        // Add parameters
+                        cmd.Parameters.AddWithValue("@username", user.Username);
+                        cmd.Parameters.AddWithValue("@password", user.Password);
+                        cmd.Parameters.AddWithValue("@email", user.Email);
+                        cmd.Parameters.AddWithValue("@firstName", user.FirstName);
+                        cmd.Parameters.AddWithValue("@lastName", user.LastName);
+                        cmd.Parameters.AddWithValue("@middleInitial", user.MiddleInitial);
+                        cmd.Parameters.AddWithValue("@role", user.Role);
+                        cmd.Parameters.AddWithValue("@department", user.Department);
+                        cmd.Parameters.AddWithValue("@studentID", user.StudentID);
+                        cmd.Parameters.AddWithValue("@employeeID", user.EmployeeID);
+                        cmd.Parameters.AddWithValue("@section", user.Section);
+                        cmd.Parameters.AddWithValue("@year", user.Year);
+                        cmd.Parameters.AddWithValue("@money", user.Money);  // Assuming 'money' is the property in your User class
+                                                                            // Add other parameters as needed
+
+                        // Execute the query
+                        cmd.ExecuteNonQuery();
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                // Handle exceptions (e.g., log the error)
+                Console.WriteLine($"Error: {ex.Message}");
+            }
+            finally
+            {
+                db.closeConnection(); // Close the database connection
+            }
+        }
+
+        // Method to check if a username is already taken
+        public bool IsUsernameTaken(string username)
+        {
+            try
+            {
+                using (MySqlConnection connection = db.getConnection())
+                {
+                    db.openConnection(); // Open the database connection
+
+                    string query = "SELECT COUNT(*) FROM app_users WHERE username = @username";
+                    using (MySqlCommand cmd = new MySqlCommand(query, connection))
+                    {
+                        cmd.Parameters.AddWithValue("@username", username);
+
+                        int count = Convert.ToInt32(cmd.ExecuteScalar());
+
+                        return count > 0;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                // Handle exceptions (e.g., log the error)
+                Console.WriteLine($"Error: {ex.Message}");
+                return true; // Assume an error, treat it as username taken
+            }
+            finally
+            {
+                db.closeConnection(); // Close the database connection
+            }
+        }
+
+        // Method to check if an employee ID is already taken
+        public bool IsEmployeeIdTaken(string employeeId)
+        {
+            try
+            {
+                using (MySqlConnection connection = db.getConnection())
+                {
+                    db.openConnection(); // Open the database connection
+
+                    string query = "SELECT COUNT(*) FROM app_users WHERE employeeID = @employeeId";
+                    using (MySqlCommand cmd = new MySqlCommand(query, connection))
+                    {
+                        cmd.Parameters.AddWithValue("@employeeId", employeeId);
+
+                        int count = Convert.ToInt32(cmd.ExecuteScalar());
+
+                        return count > 0;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                // Handle exceptions (e.g., log the error)
+                Console.WriteLine($"Error: {ex.Message}");
+                return true; // Assume an error, treat it as employee ID taken
+            }
+            finally
+            {
+                db.closeConnection(); // Close the database connection
+            }
+        }
+
+
+        // Method to check if a student ID is already taken
+        public bool IsStudentIdTaken(string studentId)
+        {
+            try
+            {
+                using (MySqlConnection connection = db.getConnection())
+                {
+                    db.openConnection(); // Open the database connection
+
+                    string query = "SELECT COUNT(*) FROM app_users WHERE studentID = @studentId";
+                    using (MySqlCommand cmd = new MySqlCommand(query, connection))
+                    {
+                        cmd.Parameters.AddWithValue("@studentId", studentId);
+
+                        int count = Convert.ToInt32(cmd.ExecuteScalar());
+
+                        return count > 0;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                // Handle exceptions (e.g., log the error)
+                Console.WriteLine($"Error: {ex.Message}");
+                return true; // Assume an error, treat it as student ID taken
+            }
+            finally
+            {
+                db.closeConnection(); // Close the database connection
+            }
+        }
     }
 }
